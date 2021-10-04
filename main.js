b86acfa5f13b93c88957afd6045c087365ae7082
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-10-04 21:05:42 +0300

    add randomPhrase - use join

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // Working with an array - Create a bad phrase generator
+// - use `+` for string
+// - use `join` 
 let bodyParts = [`nose`,`lips`,`arms`,`legs`,`body`,];
-let animals = ['chicken','elephant','monkey','rooster','ostrich']
+let animals = ['Chicken','Elephant','Monkey','Rooster','Ostrich']
 let randomWords = [`funny`,`big`,`small`]
 function random(){
     return Math.floor(Math.random()*5)
 }
+// +
 console.log(`Your `+bodyParts[random()]+` is like `+animals[random()])
-
+// join
+let randmPhrase = [`Your`,bodyParts[random()],`is like`,animals[random()]].join(" ")
+console.log(randmPhrase)
